ca4ba32e6ac8c54619ab23342eea737cd4219c5a
nammaslamma <ajhero43@gmail.com>
2024-10-13 07:15:42 -0400

    fixin stuff

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const passport = require('passport');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const nodemailer = require('nodemailer');
 const crypto = require('crypto'); 
